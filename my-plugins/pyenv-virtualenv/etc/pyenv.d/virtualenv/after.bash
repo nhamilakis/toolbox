e8817892d4243d19028ef7614728454fe56041a1
Hamilakis Nicolas <nick.hamilakis562@gmail.com>
2023-04-10 23:22:51 +0200

    removed pyenv-virtualenv:post_install_hook

--- my-plugins/pyenv-virtualenv/etc/pyenv.d/virtualenv/after.bash
+++
@@ -1,8 +0,0 @@
-# Actions permormed after creating a virtual-env 
-
-upgrade_packages() {
-  PYENV_VERSION=$VIRTUALENV_NAME pyenv-exec pip install --upgrade pip setuptools wheel
-}
-
-after_virtualenv 'upgrade_packages'
-
